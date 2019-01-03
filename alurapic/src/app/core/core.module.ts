37947c579b6f8f877809f18adba553fcf7018cf2
rsaless <rspavarina@gmail.com>
2019-01-03 01:15:06 -0200

    Criação do menu lateral e update da diretiva showIfLogged

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -7,11 +7,13 @@ import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
     declarations: [HeaderComponent, FooterComponent],
     exports: [HeaderComponent, FooterComponent],
-    imports: [CommonModule, RouterModule, AlertModule, LoadingModule],
+    imports: [CommonModule, RouterModule, AlertModule, LoadingModule, MenuModule, ShowIfLoggedModule],
     providers: [{
         provide: HTTP_INTERCEPTORS,
         useClass: RequestInterceptor,
